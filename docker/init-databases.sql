4a07cf0180c8d6054a72934d747970eaeae960b7
Singh301 <89039207+Singh301@users.noreply.github.com>
2026-09-16 09:29:23 +0530

    fix: grant Compose database user access

--- docker/init-databases.sql
+++ docker/init-databases.sql
@@ -6,3 +6,13 @@ CREATE DATABASE IF NOT EXISTS document_db;
 CREATE DATABASE IF NOT EXISTS notification_db;
 CREATE DATABASE IF NOT EXISTS audit_db;
 CREATE DATABASE IF NOT EXISTS dashboard_db;
+
+GRANT ALL PRIVILEGES ON auth_db.* TO 'loan_user'@'%';
+GRANT ALL PRIVILEGES ON customer_db.* TO 'loan_user'@'%';
+GRANT ALL PRIVILEGES ON loan_db.* TO 'loan_user'@'%';
+GRANT ALL PRIVILEGES ON repayment_db.* TO 'loan_user'@'%';
+GRANT ALL PRIVILEGES ON document_db.* TO 'loan_user'@'%';
+GRANT ALL PRIVILEGES ON notification_db.* TO 'loan_user'@'%';
+GRANT ALL PRIVILEGES ON audit_db.* TO 'loan_user'@'%';
+GRANT ALL PRIVILEGES ON dashboard_db.* TO 'loan_user'@'%';
+FLUSH PRIVILEGES;
