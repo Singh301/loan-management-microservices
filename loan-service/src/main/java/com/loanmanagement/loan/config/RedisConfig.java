eab6b21e4cea2b4bf01750aff227cb94614e316f
Singh301 <89039207+Singh301@users.noreply.github.com>
2026-09-16 08:22:54 +0530

    feat(phase2): enable Redis caching for loan service

---
+++ loan-service/src/main/java/com/loanmanagement/loan/config/RedisConfig.java
@@ -0,0 +1,9 @@
+package com.loanmanagement.loan.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+}
