60c99705d82a978e205d15b930094906acfd5a57
Singh301 <89039207+Singh301@users.noreply.github.com>
2026-09-16 09:28:20 +0530

    fix: remove duplicate correlation filter bean

--- dashboard-service/src/main/java/com/loanmanagement/dashboard/DashboardServiceApplication.java
+++ dashboard-service/src/main/java/com/loanmanagement/dashboard/DashboardServiceApplication.java
@@ -1,13 +1,10 @@
 package com.loanmanagement.dashboard;
 
-import com.loanmanagement.common.security.CorrelationIdFilter;
 import com.loanmanagement.common.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"com.loanmanagement.dashboard", "com.loanmanagement.common"})
 @EnableDiscoveryClient
@@ -15,14 +12,5 @@ import org.springframework.context.annotation.Bean;
 public class DashboardServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DashboardServiceApplication.class, args);
-    }
-
-    @Bean
-    FilterRegistrationBean<CorrelationIdFilter> correlationIdFilter() {
-        FilterRegistrationBean<CorrelationIdFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new CorrelationIdFilter());
-        registration.addUrlPatterns("/*");
-        registration.setOrder(Integer.MIN_VALUE);
-        return registration;
     }
 }
