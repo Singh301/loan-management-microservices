d6c10bf793a2538e0acc1c3ea62c912b29f1bae5
Singh301 <89039207+Singh301@users.noreply.github.com>
2026-09-16 09:28:29 +0530

    fix: remove duplicate correlation filter bean

--- document-service/src/main/java/com/loanmanagement/document/DocumentServiceApplication.java
+++ document-service/src/main/java/com/loanmanagement/document/DocumentServiceApplication.java
@@ -1,13 +1,10 @@
 package com.loanmanagement.document;
 
-import com.loanmanagement.common.security.CorrelationIdFilter;
 import com.loanmanagement.common.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"com.loanmanagement.document", "com.loanmanagement.common"})
 @EnableDiscoveryClient
@@ -15,14 +12,5 @@ import org.springframework.context.annotation.Bean;
 public class DocumentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DocumentServiceApplication.class, args);
-    }
-
-    @Bean
-    FilterRegistrationBean<CorrelationIdFilter> correlationIdFilter() {
-        FilterRegistrationBean<CorrelationIdFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new CorrelationIdFilter());
-        registration.addUrlPatterns("/*");
-        registration.setOrder(Integer.MIN_VALUE);
-        return registration;
     }
 }
