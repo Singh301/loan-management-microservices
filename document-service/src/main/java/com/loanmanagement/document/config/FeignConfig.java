0232a2eb9c47ff14bb0823a17612200bf9243001
Singh301 <89039207+Singh301@users.noreply.github.com>
2026-09-18 12:00:32 +0530

    security: forward authenticated context for document Feign calls

---
+++ document-service/src/main/java/com/loanmanagement/document/config/FeignConfig.java
@@ -0,0 +1,33 @@
+package com.loanmanagement.document.config;
+
+import com.loanmanagement.common.constants.ApiConstants;
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor authenticatedContextForwardingInterceptor() {
+        return template -> {
+            if (!(RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes attributes)) {
+                return;
+            }
+
+            var request = attributes.getRequest();
+            String authorization = request.getHeader("Authorization");
+            if (StringUtils.hasText(authorization)) {
+                template.header("Authorization", authorization);
+            }
+
+            String requestId = request.getHeader(ApiConstants.HEADER_CORRELATION_ID);
+            if (StringUtils.hasText(requestId)) {
+                template.header(ApiConstants.HEADER_CORRELATION_ID, requestId);
+            }
+        };
+    }
+}
