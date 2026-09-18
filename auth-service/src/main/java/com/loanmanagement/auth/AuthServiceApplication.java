82d25510ef42fb5d14b7e32e39add5465f7051a2
Singh301 <89039207+Singh301@users.noreply.github.com>
2026-09-18 11:36:41 +0530

    auth: enable scheduled refresh token cleanup

--- auth-service/src/main/java/com/loanmanagement/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/loanmanagement/auth/AuthServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.loanmanagement.auth", "com.loanmanagement.common"})
 @EnableDiscoveryClient
+@EnableScheduling
 @EnableConfigurationProperties(JwtProperties.class)
 public class AuthServiceApplication {
     public static void main(String[] args) {
