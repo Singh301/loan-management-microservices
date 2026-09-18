42e45e2c0ae8849b7f0e701b6e5800956c2a5789
Singh301 <89039207+Singh301@users.noreply.github.com>
2026-09-18 12:06:07 +0530

    storage: configure S3 client with IAM credentials

---
+++ document-service/src/main/java/com/loanmanagement/document/config/StorageConfig.java
@@ -0,0 +1,37 @@
+package com.loanmanagement.document.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Configuration
+public class StorageConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "document.storage.type", havingValue = "s3")
+    public S3Client s3Client(
+            @Value("${document.storage.s3.region}") String region,
+            @Value("${document.storage.s3.endpoint:}") String endpoint,
+            @Value("${document.storage.s3.path-style-access:false}") boolean pathStyleAccess) {
+
+        var builder = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(pathStyleAccess)
+                        .build());
+
+        if (endpoint != null && !endpoint.isBlank()) {
+            builder.endpointOverride(URI.create(endpoint));
+        }
+
+        return builder.build();
+    }
+}
