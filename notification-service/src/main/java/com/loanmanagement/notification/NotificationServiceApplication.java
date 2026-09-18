db4cffb23bc4461269715fe673527dba773359ce
Singh301 <89039207+Singh301@users.noreply.github.com>
2026-09-18 11:59:41 +0530

    security: enable Feign in notification service

--- notification-service/src/main/java/com/loanmanagement/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/loanmanagement/notification/NotificationServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication(scanBasePackages = {"com.loanmanagement.notification", "com.loanmanagement.common"})
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableKafka
 @EnableConfigurationProperties(JwtProperties.class)
 public class NotificationServiceApplication {
