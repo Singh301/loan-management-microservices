9b0f2240a8a379e9526f8fe96d4767dc48e25a85
Singh301 <89039207+Singh301@users.noreply.github.com>
2026-09-18 12:00:30 +0530

    security: enable Feign in document service

--- document-service/src/main/java/com/loanmanagement/document/DocumentServiceApplication.java
+++ document-service/src/main/java/com/loanmanagement/document/DocumentServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.loanmanagement.document", "com.loanmanagement.common"})
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableConfigurationProperties(JwtProperties.class)
 public class DocumentServiceApplication {
     public static void main(String[] args) {
